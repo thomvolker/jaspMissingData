b262749d59ef6f5670cac18e94ba12ad797c18ab
Thom Volker <t.b.volker@uu.nl>
2025-05-15 11:48:10 +0200

    Complete test-process-imputation-options

--- tests/testthat/test-process-imputation-options.R
+++ tests/testthat/test-process-imputation-options.R
@@ -1,13 +1,13 @@
-test_that(".processImputationOptions", {
-  options <- list(
-    imputationVariables = list(
-      optionKey = "variable",
-      types = c("scale", "nominal", "scale"),
-      value = list(
-        list(method = "pmm", variable = "hgt"),
-        list(method = "logreg", variable = "reg"),
-        list(method = "pmm", variable = "bmi")
-      )
+test_that(".processImputationOptions returns expected output", {
+
+  options <- jaspTools::analysisOptions("MissingDataImputation")
+  options$imputationVariables <- list(
+    optionKey = "variable",
+    types = c("scale", "nominal", "scale"),
+    value = list(
+      list(method = "pmm", variable = "hgt"),
+      list(method = "logreg", variable = "reg"),
+      list(method = "pmm", variable = "bmi")
     )
   )
   expect_equal(
@@ -18,5 +18,16 @@ test_that(".processImputationOptions", {
     .processImputationOptions(options)$imputationMethods,
     c(hgt = "pmm", reg = "logreg", bmi = "pmm")
   )
-
+  expect_equal(
+    .processImputationOptions(options)$imputedVariables,
+    ""
+  )
+  expect_equal(
+    .processImputationOptions(options)$fType,
+    1
+  )
+  expect_equal(
+    .processImputationOptions(options)$lmFunction,
+    pooledLm
+  )
 })
