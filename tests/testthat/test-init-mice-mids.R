a00673d8567595d0892ef2bdd97e2653b2fb09b2
Thom Volker <t.b.volker@uu.nl>
2025-07-10 13:13:04 +0200

    update test .initMiceMids

--- tests/testthat/test-init-mice-mids.R
+++ tests/testthat/test-init-mice-mids.R
@@ -3,7 +3,8 @@ test_that("initMiceMids runs without errors", {
   expect_null(
     .initMiceMids(list(MiceMids = "notnull"))
   )
-  expect_error(
-    .initMiceMids(NULL)
+  expect_type(
+    .initMiceMids(NULL),
+    "environment"
   )
 })
