12c55c0e48bbe01721a3be1883ddc1b4a9657aca
Thom Volker <t.b.volker@uu.nl>
2025-07-10 13:26:48 +0200

    remove bmi from test

--- tests/testthat/test-method-specification.R
+++ tests/testthat/test-method-specification.R
@@ -1,18 +1,18 @@
 
 test_that(".makeMethodVector works", {
-  data <- mice::boys[,c("hgt", "wgt", "bmi")]
+  data <- mice::boys[,c("hgt", "wgt", "gen")]
   options <- list(
-    imputationTargets = c("hgt", "wgt", "bmi"),
-    imputationMethods = c("pmm", "pmm", "pmm")
+    imputationTargets = c("hgt", "wgt", "gen"),
+    imputationMethods = c("pmm", "pmm", "logistic")
   )
   expect_equal(
     .makeMethodVector(data, options),
-    c("pmm", "pmm", "pmm") |> setNames(colnames(data))
+    c("pmm", "pmm", "polyreg") |> setNames(colnames(data))
   )
-  options$imputationMethods <- c("pmm", "norm", "norm")
+  options$imputationMethods <- c("pmm", "norm", "pmm")
   expect_equal(
     .makeMethodVector(data, options),
-    c("pmm", "norm", "norm") |> setNames(colnames(data))
+    c("pmm", "norm", "pmm") |> setNames(colnames(data))
   )
   options$imputationMethods <- c("pmm", "logistic", "logistic")
   expect_equal(
