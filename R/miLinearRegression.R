ec82af65ddb0320bb1453ec520f35920d91fdde6
Kyle M. Lang <kylelang86@gmail.com>
2026-05-07 17:32:36 +0200

    implmented what looks like correct pooling for the difference stats in the summary table

--- R/miLinearRegression.R
+++ R/miLinearRegression.R
@@ -15,31 +15,41 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 .runRegression <- function(jaspResults, options, ready, lmFunction) {
-
   impData <- jaspResults[["MiceMids"]]$object |> mice::complete("all")
 
   # modelContainer <- .linregGetModelContainer(jaspResults, position = 1)
   modelContainer <- jaspResults[["ModelContainer"]]
-  model          <- .linregCalcModel(modelContainer, impData, options, ready, lmFunction)
+  model <- jaspRegression:::.linregCalcModel(modelContainer, impData, options, ready, lmFunction)
 
-  if (is.null(modelContainer[["summaryTable"]]))
-    .linregCreateSummaryTable(modelContainer, model, options, position = 1)
+  model[[1]][["rSquareChange"]] <- .pooledRSquaredChange(fit1 = model[[1]]$fit) # , fStat = options$fStat)
+
+  for (i in seq_along(model)[-1]) {
+    model[[i]][["rSquareChange"]] <-
+      .pooledRSquaredChange(fit1 = model[[i]]$fit, fit0 = model[[i - 1]]$fit) # , fStat = options$fStat)
+    # durbinWatson  <- model[[i]][["durbinWatson"]]
+  }
+
+  if (is.null(modelContainer[["summaryTable"]])) {
+    jaspRegression:::.linregCreateSummaryTable(modelContainer, model, options, position = 1)
+  }
 
   # TODO (KML): Check the R2, F, AIC/BIC stuff and put MI appropriate versions in
   # TODO (KML): Add footnotes about pooling
- 
-  if (options$modelFit && is.null(modelContainer[["anovaTable"]]))
-    .linregCreateAnovaTable(modelContainer, model, options, position = 2)
+
+  if (options$modelFit && is.null(modelContainer[["anovaTable"]])) {
+    jaspRegression:::.linregCreateAnovaTable(modelContainer, model, options, position = 2)
+  }
 
   # TODO (KML): Check the ANOVA stats.
   #             - Are they correct for MI data
   #             - Do they correctly adjust for D1, D2, D3?
 
-  if (options$coefficientEstimate && is.null(modelContainer[["coeffTable"]]))
-    .linregCreateCoefficientsTable(modelContainer, model, impData, options, position = 3)
+  if (options$coefficientEstimate && is.null(modelContainer[["coeffTable"]])) {
+    jaspRegression:::.linregCreateCoefficientsTable(modelContainer, model, impData, options, position = 3)
+  }
 
   # TODO (KML): Check what we can do about the bootstrapping, partial cor, and collinearity tables
 
@@ -49,14 +59,49 @@
   # if (options$partAndPartialCorrelation && is.null(modelContainer[["partialCorTable"]]))
   #   jaspRegression:::.linregCreatePartialCorrelationsTable(modelContainer, model, dataset, options, position = 6)
 
-  if (options$covarianceMatrix && is.null(modelContainer[["coeffCovMatrixTable"]]))
-    .linregCreateCoefficientsCovarianceMatrixTable(modelContainer, model, options, position = 4)
+  if (options$covarianceMatrix && is.null(modelContainer[["coeffCovMatrixTable"]])) {
+    jaspRegression:::.linregCreateCoefficientsCovarianceMatrixTable(modelContainer, model, options, position = 4)
+  }
 
   # if (options$collinearityDiagnostic && is.null(modelContainer[["collinearityTable"]]))
   #   jaspRegression:::.linregCreateCollinearityDiagnosticsTable(modelContainer, model, options, position = 8)
 }
 
 ## Execute .runRegression() within the 'jaspRegression' namespace:
-environment(.runRegression) <- asNamespace('jaspRegression')
+# environment(.runRegression) <- asNamespace("jaspRegression")
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
+
+.pooledRSquaredChange <- function(fit1, fit0 = NULL) {
+  # fFun <- switch(fStat,
+  #   d1 = mice::D1,
+  #   d2 = mice::D2,
+  #   d3 = mice::D3
+  # )
+  #
+  if (is.null(fit0)) {
+    out <- list(
+      R2c = NA,
+      Fc  = NA,
+      df1 = 0,
+      df2 = fit1$df.residual,
+      p   = NA
+    )
+  } else {
+    fOut <- fit1$fFun(fit1 = fit1$fits, fit0 = fit0$fits)
+
+    out <- list(
+      R2c = fit1$pooled$r2[1, "est"] - fit0$pooled$r2[1, "est"],
+      Fc  = fOut$result[[1]],
+      df1 = fOut$result[[2]],
+      df2 = fOut$result[[3]],
+      p   = fOut$result[[4]]
+    )
+  }
+
+  out
+}
+
+# environment(.pooledRSquaredChange) <- asNamespace("jaspRegression")
+
+### ------------------------------------------------------------------------------------------------------------------###
--- R/pooledLm.R
+++ R/pooledLm.R
@@ -15,71 +15,76 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 #' @export
 makePooledLm <- function(pool, poolingParams) {
   function(formula, data, weights, ...) {
-
     ## TIL that formula objects are closures that capture the environment in which they are created. So, we need to create
     ## the formula object in an environment containing 'weights', otherwise lm() won't find 'weights'.
     form <- as.formula(formula)
     environment(form) <- environment()
 
     fits <- vector("list", length(data))
-    for(m in 1:length(data))
+    for (m in 1:length(data)) {
       fits[[m]] <- lm(form, data = data[[m]], weights = weights[[m]], ...)
+    }
 
-    if(!pool) return(fits)
+    if (!pool) {
+      return(fits)
+    }
 
     pooledLmObject(fits, pooling = poolingParams)
   }
 }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 #' @export
 pooledLmObject <- function(fits,
                            pooling = list(fStat = "d3", llEst = "qBar"),
-                           include = list(model = FALSE, qr = FALSE, x = FALSE)
-                           )
-{
+                           include = list(fits = TRUE, model = FALSE, qr = FALSE, x = FALSE)) {
   fits <- .checkInputs(fits, pooling)
 
   ## Much of the lm object doesn't change across imputed datasets, so we can keep one of the original fits and replace
   ## the appropriate parts with MI-based estimates.
-  obj        <- fits$analyses[[1]]
+  obj <- fits$analyses[[1]]
   class(obj) <- c("pooledlm", class(obj))
 
   ## We need to extract these things before casting the 'fits' as a mira object:
   residuals <- sapply(fits$analyses, resid)
-  yHats     <- sapply(fits$analyses, fitted)
-  effects   <- sapply(fits$analyses, effects)
+  yHats <- sapply(fits$analyses, fitted)
+  effects <- sapply(fits$analyses, effects)
 
   ## The pooled estimates of linear effects are just the arithmetic mean across imputations:
-  obj$residuals     <- rowMeans(residuals)
+  obj$residuals <- rowMeans(residuals)
   obj$fitted.values <- rowMeans(yHats)
-  obj$effects       <- rowMeans(effects)
+  obj$effects <- rowMeans(effects)
 
   obj$singularityCheckValue <- sapply(fits$analyses, function(x) x$qr$qr |> ncol()) |> min()
 
   ## Do we keep the stuff we can't pool?
-  if(include$model) obj$model <- lapply(fits$analyses, "[[", x = "model") else obj$model <- NA
-  if(include$qr)    obj$qr    <- lapply(fits$analyses, "[[", x = "qr")    else obj$qr    <- NA
-  if(include$x)     obj$x     <- lapply(fits$analyses, "[[", x = "x")     else obj$x     <- NA
+  if (include$fits) obj$fits <- fits
+  if (include$model) obj$model <- lapply(fits$analyses, "[[", x = "model") else obj$model <- NA
+  if (include$qr) obj$qr <- lapply(fits$analyses, "[[", x = "qr") else obj$qr <- NA
+  if (include$x) obj$x <- lapply(fits$analyses, "[[", x = "x") else obj$x <- NA
 
   ## Use the mice pooling routines for the more complicated cases:
-  pooled      <- list()
+  pooled <- list()
   pooled$coef <- mice::pool(fits)
-  pooled$r2   <- mice::pool.r.squared(fits)
-  pooled$r2A  <- mice::pool.r.squared(fits, adjusted = TRUE)
+  pooled$r2 <- mice::pool.r.squared(fits)
+  pooled$r2A <- mice::pool.r.squared(fits, adjusted = TRUE)
 
   if (obj$rank > 1) { # We can only compute pooled F when we have some predictors
-    fFun            <- switch(pooling$fStat, d1 = mice::D1, d2 = mice::D2, d3 = mice::D3)
-    pooled$f        <- fFun(fits)
+    obj$fFun <- switch(pooling$fStat,
+      d1 = mice::D1,
+      d2 = mice::D2,
+      d3 = mice::D3
+    )
+    pooled$f <- obj$fFun(fits)
     obj$df.residual <- as.numeric(pooled$f$result)[3]
   } else if (obj$rank == 1) { # We're pooling an intercept-only model
-    pooled$f        <- NULL
+    pooled$f <- NULL
     obj$df.residual <- pooled$coef$pooled$df
   } else {
     stop("Wow! Something has gone terribly wrong. You should not be able to access this condition.")
@@ -88,20 +93,21 @@ pooledLmObject <- function(fits,
   pooled$logLik <- .pooledLogLik(fits, est = pooling$llEst, pool = TRUE)
 
   ## Replace pool-able stats with their pooled versions
-  obj$coefficients        <- pooled$coef$pooled$estimate
+  obj$coefficients <- pooled$coef$pooled$estimate
   names(obj$coefficients) <- as.character(pooled$coef$pooled$term)
 
   ## Calculate the pooled residual variance:
-  resVars   <- pooled$coef$glanced$sigma^2
-  w         <- mean(resVars)
-  b         <- var(resVars)
+  resVars <- pooled$coef$glanced$sigma^2
+  w <- mean(resVars)
+  b <- var(resVars)
   pooled$s2 <- w + b + (b / length(fits))
 
   ## Calculate the pooled asymptotic covariance matrix of the regression coefficients:
-  if(obj$rank > 1)
+  if (obj$rank > 1) {
     pooled$vcov <- .pooledAsymptoticCov(fits$analyses)
-  else
+  } else {
     pooled$vcov <- with(pooled$coef$pooled, matrix(ubar + b + (b / m), dimnames = list(term, term)))
+  }
 
   ## Include the extra pooled stats we'll need downstream
   obj$pooled <- pooled
@@ -109,11 +115,12 @@ pooledLmObject <- function(fits,
   obj
 }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 .checkInputs <- function(fits, pooling) {
-  if (!(mice::is.mira(fits) || is.list(fits)))
+  if (!(mice::is.mira(fits) || is.list(fits))) {
     stop("The 'fits' argument must be a a 'mira' object or list of fitted lm models.")
+  }
 
   if (!mice::is.mira(fits)) {
     fitsAreLm <- sapply(fits, inherits, what = "lm")
@@ -123,10 +130,12 @@ pooledLmObject <- function(fits,
     fits <- mice::as.mira(fits)
   }
 
-  if (!pooling$fStat %in% paste0("d", 1:3))
+  if (!pooling$fStat %in% paste0("d", 1:3)) {
     stop("The pooling type for F-statistics must be 'd1', 'd2', or 'd3'.")
-  if (!pooling$llEst %in% c("qBar", "qHat"))
+  }
+  if (!pooling$llEst %in% c("qBar", "qHat")) {
     stop("The estimate type at which to evaluate the log-likelihood must be 'qBar' or 'qHat'.")
+  }
 
   ranks <- sapply(fits$analyses, "[[", x = "rank")
   if (any(ranks <= 0)) stop("I don't know how to pool models with rank(X) < 1.")
@@ -134,28 +143,30 @@ pooledLmObject <- function(fits,
   fits
 }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 #' @export
 summary.pooledlm <- function(object, ...) {
   out <- structure(list(), class = c("summary.pooledlm", "summary.lm")) # Maybe we can just use the print method for summary.lm ?
 
   ## We can pull a few things directly from the input object
-  out$call      <- object$call
+  out$call <- object$call
   out$residuals <- object$residuals
-  out$terms     <- object$terms
-  out$weights   <- object$weights
+  out$terms <- object$terms
+  out$weights <- object$weights
 
   ## Tidy up the fit related stuff
-  out$r.squared     <- object$pooled$r2[1, "est"]
+  out$r.squared <- object$pooled$r2[1, "est"]
   out$adj.r.squared <- object$pooled$r2A[1, "est"]
-  out$sigma         <- object$pooled$s2 |> sqrt()
+  out$sigma <- object$pooled$s2 |> sqrt()
 
   ## Reverse engineer the unscaled covariance matrix:
   out$cov.unscaled <- with(object$pooled, vcov / s2)
 
   if (object$rank > 1) {
-    f <- object$pooled$f$result |> as.numeric() |> head(3)
+    f <- object$pooled$f$result |>
+      as.numeric() |>
+      head(3)
     names(f) <- c("value", "numdf", "dendf")
     out$fstatistic <- f
   }
@@ -164,33 +175,35 @@ summary.pooledlm <- function(object, ...) {
   pooledCoefSum <- summary(object$pooled$coef)
 
   ## The coefficients table needs formatting
-  coefTab <- with(pooledCoefSum,
+  coefTab <- with(
+    pooledCoefSum,
     cbind("Estimate" = estimate, "Std. Error" = std.error, "t value" = statistic, "Pr(>|t|)" = p.value)
   )
   rownames(coefTab) <- pooledCoefSum$term
-  out$coefficients  <- coefTab
+  out$coefficients <- coefTab
 
   ## Some values used for printing
   out$aliased <- is.na(object$coefficients)
-  out$df      <- with(object, c(rank, df.residual, singularityCheckValue))
+  out$df <- with(object, c(rank, df.residual, singularityCheckValue))
 
   invisible(out)
 }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 #' @export
-print.summary.pooledlm <- function(object, allowStarGazing = FALSE, ...)
+print.summary.pooledlm <- function(object, allowStarGazing = FALSE, ...) {
   stats:::print.summary.lm(object, signif.stars = allowStarGazing, ...)
+}
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 #' @export
-coef.pooledlm   <- function(object) object$coefficients
+coef.pooledlm <- function(object) object$coefficients
 #' @export
-vcov.pooledlm   <- function(object) object$pooled$vcov
+vcov.pooledlm <- function(object) object$pooled$vcov
 #' @export
-resid.pooledlm  <- function(object) object$residuals
+resid.pooledlm <- function(object) object$residuals
 #' @export
 fitted.pooledlm <- function(object) object$fitted.values
 #' @export
@@ -200,16 +213,18 @@ logLik.pooledlm <- function(object) object$pooled$logLik
 #' @export
 # BIC.pooledlm    <- function(object, ...) object$pooled$infoCriteria$bic
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 .pooledAsymptoticCov <- function(fits) {
   m <- length(fits)
 
-  coefs     <- sapply(fits, coef)
+  coefs <- sapply(fits, coef)
   coefNames <- rownames(coefs)
 
   ## Between-imputation var/covar:
-  b <- coefs |> t() |> var()
+  b <- coefs |>
+    t() |>
+    var()
 
   ## Average within-imputation var/covar:
   w <- sapply(fits, vcov) |>
@@ -220,7 +235,7 @@ logLik.pooledlm <- function(object) object$pooled$logLik
   w + b + (b / m)
 }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 .pooledLogLik <- function(fits, est = "qBar", pool = TRUE) {
   if (est == "qBar") {
@@ -234,13 +249,13 @@ logLik.pooledlm <- function(object) object$pooled$logLik
   if (pool) ll <- mean(ll)
 
   attr(ll, "nobs") <- fits$analyses[[1]]$residuals |> length()
-  attr(ll, "df")   <- fits$analyses[[1]]$rank + 1
-  class(ll)        <- "logLik"
+  attr(ll, "df") <- fits$analyses[[1]]$rank + 1
+  class(ll) <- "logLik"
 
   ll
 }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
 
 ## Pool AIC and BIC via naive averaging or the "Pooled Paramters - Pooled Log-Likelihoods" method proposed in
 ## https://doi.org/10.1007/s41237-025-00281-6
@@ -264,4 +279,4 @@ logLik.pooledlm <- function(object) object$pooled$logLik
 #   list(aic = aic, bic = bic, p = p, n = n, type = type)
 # }
 
-###------------------------------------------------------------------------------------------------------------------###
+### ------------------------------------------------------------------------------------------------------------------###
